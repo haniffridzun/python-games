d3d71117218d6fddf371ea92e8a4841a77765192
haniffridzun <haniffridzun@gmail.com>
2023-03-07 21:14:32 +0800

    Check for winning conditions

--- connect-four.py
+++ connect-four.py
@@ -14,7 +14,7 @@ def drop_piece(board, row, col, piece):
 
 
 def is_valid_loc(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT - 1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -25,6 +25,29 @@ def get_next_open_row(board, col):
 
 def print_board(board):
     print(np.flip(board, 0))
+
+
+def winning_move(board, piece):
+    # check horizontal location for win
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    # check vertical location for win
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+    # check positive slope
+    for c in range(COL_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    # check negative slope
+    for c in range(COL_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
 
 
 board = create_board()
@@ -39,13 +62,23 @@ while not game_over:
         if is_valid_loc(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+            # winning condition
+            if winning_move(board, 1):
+                print('player 1 wins!')
+                game_over = True
+                break
     # ask player2 input
     else:
         col = int(input("player 2 make selection (0-6):"))
         if is_valid_loc(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+            # winning condition
+            if winning_move(board, 2):
+                print('player 2 wins!')
+                game_over = True
+                break
 
+    print_board(board)
     turn += 1
     turn = turn % 2
-    print_board(board)
